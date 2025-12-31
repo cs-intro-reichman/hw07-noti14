f8977a01d343a49dec07012da0bedb4bceb8753b
yoav nothmann <yoav.nothmann@post.runi.ac.il>
2025-12-31 17:52:20 +0200

    complete

--- Binomial.java
+++ Binomial.java
@@ -14,34 +14,29 @@ public class Binomial {
 	 } 
 	
 	// Computes the Binomial function, efficiently
-	public static int binomial(int n, int k) {
+	public static long binomial(int n, int k) {
 		if(k > n) return 0;
 		if(k == 0 || n == 0) return 1;
 		
-		int [][] memo = new int[n + 1][k + 1];
-		for(int i = 0; i < n; i ++) {
-			for(int j = 0; j < k; j ++) {
+		long [][] memo = new long[n + 1][k + 1];
+		for(int i = 0; i <= n; i ++) {
+			for(int j = 0; j <= k; j ++) {
 				memo[i][j] = -1;
 			}
 		}
 		return binomial(n, k, memo);
 	}
 
-	private static int binomial(int n, int k, int[][] memo) {
-		if (memo[n][k] != -1) {
+	private static long binomial(int n, int k, long[][] memo) {
+		
+		if (k > n) return 0;
+		if (k == 0 || n == k) return 1;
+		
+		if(memo[n][k] != -1) {
 			return memo[n][k];
 		}
-		// Base case
-		if ((k > n)) {
-		   	memo[n][k] = 0; 
-		   	return 0;
-		}
-		// Another base case
-		if (n == 0 || k == 0) {
-		   	memo[n][k] = 1; 
-		   	return 1;
-		}
-		memo[n][k] = binomial(n - 1, k, memo) + binomial(n - 1, k - 1, memo);
+
+		memo[n][k] = binomial( n - 1, k, memo) + binomial(n - 1, k - 1, memo);
 		return memo[n][k];
 	}
 }
